b04a5834a8a97d781e225d30b6d5f2c8b8798a50
leo <leo@iris-advies.com>
2021-12-05 16:30:25 +0100

    weer wat characters minder

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,12 @@
 from itertools import*
 from collections import*
 import re
-r=lambda a,b:cycle([a])if a==b else range(a,b+(c:=(a<b)*2-1),c)
-print(sum(a>1 for a in Counter(chain(*[zip(r(c[0],c[2]),r(c[1],c[3]))for c in [list(map(int,re.split(",| -> ",l)))for l in open("i","r").readlines()]])).values()))
+r=lambda a,b:range(a,b+(c:=(a<b)*2-1),c)if a-b else[a]*9**4
+print(sum(a>1for a in Counter(chain(*[zip(r(*c[::2]),r(*c[1::2]))for c in[list(map(int,re.split(",| -> ",l)))for l in open("i")]])).values()))
+
+from itertools import*
+import re
+r=lambda a,b:range(a,b+(c:=(a<b)*2-1),c)if a-b else[a]*9**4
+c=list(chain(*[zip(r(*c[::2]),r(*c[1::2]))for c in[list(map(int,re.split(",| -> ",l)))for l in open("i")]]))
+print(sum(c.count(v)>1for v in set(c)))
+
